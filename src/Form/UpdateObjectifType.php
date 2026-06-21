5648ea06ee8bbcab9493b043606e47b3cbe55b34
Ylan <turin-ylan@outlook.fr>
2026-06-21 11:31:10 +0200

    update UpdateObjectifType to use DateTimeType from Symfony form extension

--- src/Form/UpdateObjectifType.php
+++ src/Form/UpdateObjectifType.php
@@ -4,10 +4,10 @@ namespace App\Form;
 
 use App\Entity\Objectif;
 use App\Entity\User;
-use Doctrine\DBAL\Types\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
